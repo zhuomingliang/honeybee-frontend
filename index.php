42734ec7c75a4f8c54f397703a4ce3e673fc6352
Jimmy Zhuo <zhuomingliang@yahoo.com.cn>
2010-07-30 21:32:46 +0800

    remove debug code

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 require_once 'Honeybee/config/lib.config.php';
 require_once 'Honeybee/lib/Class/Core/Controller/AppController.php';
+
 require_once 'Config/lib.config.php';
 
 
-require_once 'Honeybee/lib/Class/Core/Model/Mongo/Product.php';
-require_once 'Honeybee/lib/Class/Core/Model/Mongo/Tag.php';
+#require_once 'Honeybee/lib/Class/Core/Model/Mongo/Product.php';
+#require_once 'Honeybee/lib/Class/Core/Model/Mongo/Tag.php';
 #require_once 'lib/Class/Base/Config.php';
 
 AppController::createApp()->run();
-
+/*
 $tag = new Tag();
 $tag->remove();
 $tag->save(array('name' => 'LV', 'count' => 20));
@@ -20,4 +21,5 @@ $tag->save(array('name' => 'IBM', 'count' => 22));
 foreach($tag->getHotTags(3) as $_tag) {
    var_dump($_tag);
 }
+*/
 ?>
