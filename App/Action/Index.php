fb775547eaca8081886a74d3ed1ec160fada3b77
Jimmy Zhuo <zhuomingliang@yahoo.com.cn>
2011-10-21 17:58:24 +0800

    add fixes to route

--- App/Action/Index.php
+++ App/Action/Index.php
@@ -2,7 +2,7 @@
 
 class IndexAction {
     public function __construct($path_info) {
-
+        echo $path_info;
     }
 }
 
--- App/Controller/index.php
+++ App/Controller/index.php
@@ -1,11 +1,5 @@
 <?php
 Class IndexController extends Controller {
-    public function __construct(){
-        parent::__construct();
-
-        echo 'hello';
-    }
-
     public function route($controller_name = 'Index'){
         parent::route($controller_name);
     }
