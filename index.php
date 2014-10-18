04b5f03c061ea106c3c81a7a6c1aa9e291e81c25
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-10-18 17:03:59 +0800

    fix

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 define('APP_DIR', dirname(__FILE__) . '/');
 
-require 'Honeybee/config/lib.config.php';
-
 require APP_DIR . 'Config/lib.config.php';
 require APP_DIR . 'Config/app.config.php';
 
