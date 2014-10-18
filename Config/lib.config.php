a3208e407830996a0f09d15523c304a009a98ae5
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-10-18 17:04:55 +0800

    fix

--- Config/lib.config.php
+++ Config/lib.config.php
@@ -1,5 +1,8 @@
 <?php
-require_once HONEYBEE_DIR . 'lib/Class/Core/Base/Log.php';
+define('HONEYBEE_DIR', '/www/htm/Honeybee/honeybee/');
+
+require HONEYBEE_DIR . 'config/lib.config.php';
+require HONEYBEE_DIR . 'lib/Class/Core/Base/Log.php';
 
 set_exception_handler('Log::save');
 ?>
