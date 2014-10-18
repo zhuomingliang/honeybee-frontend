3426c43a3ef89fa072e8823e0172069b6f6d5fd3
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-10-18 17:05:34 +0800

    fix

--- Config/lib.config.php
+++ Config/lib.config.php
@@ -4,5 +4,5 @@ define('HONEYBEE_DIR', '/www/htm/Honeybee/honeybee/');
 require HONEYBEE_DIR . 'config/lib.config.php';
 require HONEYBEE_DIR . 'lib/Class/Core/Base/Log.php';
 
-set_exception_handler('Log::save');
+// set_exception_handler('Log::save');    # 如果是开发，请把这行代码注释，并安装 Xdebug，方便调试
 ?>
